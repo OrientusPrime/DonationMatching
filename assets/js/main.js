a8071a63b6e17a05f8f826133405ee163fb556f6
Heval Nergiz <1hevalnergiz1@gmail.com>
2023-02-13 00:59:57 +0300

    Sum bugs are fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -769,7 +769,7 @@ async function searchOnNewPage() {
                     <div class="pool-card-header-div-mother">
                         <div class="pool-card-header-div">
                             <h4 class="poolname"> </h4>
-                            <div class="remain-day-div"><span class="remain-day-span">15</span><span>days left</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1em" height="1em" fill="currentColor">
+                            <div class="remain-day-div"><span class="remain-day-span">x</span><span>days left</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1em" height="1em" fill="currentColor">
                                     <!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. -->
                                     <path d="M232 120C232 106.7 242.7 96 256 96C269.3 96 280 106.7 280 120V243.2L365.3 300C376.3 307.4 379.3 322.3 371.1 333.3C364.6 344.3 349.7 347.3 338.7 339.1L242.7 275.1C236 271.5 232 264 232 255.1L232 120zM256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0zM48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48C141.1 48 48 141.1 48 256z"></path>
                                 </svg></div>
@@ -919,6 +919,19 @@ async function increaseDeadline(poolId, increaseDeadlineAmount) {
   console.log(`Transaction hash: ${tx.hash}`);
 }
 
+async function increaseDeadlineByUser() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const encodedName = urlParams.get("poolId");
+  const deadlineInput = document.getElementById("newdeadline");
+  let newDeadlineDate = new Date(deadlineInput.value);
+
+  let today = new Date();
+
+  let diffDays = newDeadlineDate.getTime() - today.getTime();
+  increaseDeadline(encodedName, diffDays);
+  console.log(` ${newDeadlineDate} deadline increased`);
+}
+
 // Call the increaseMaxMatch function
 async function increaseMaxMatch(poolId, increaseMaxMatchAmount) {
   const tx = await ourContract.increaseMaxMatch(poolId, increaseMaxMatchAmount);
@@ -929,8 +942,15 @@ async function increaseMaxMatchByUser() {
   const urlParams = new URLSearchParams(window.location.search);
   const encodedName = urlParams.get("poolId");
   let newMatchAmountInput = document.getElementById("newmatchamount");
-  increaseMaxMatch(encodedName, newMatchAmountInput.value);
-  console.log(` ${newMatchAmountInput.value} amount increased for matching`);
+
+  let allowanceAmount = await getAllowance(walletAddress, ourContractAddress);
+  let lastAllowedAmount = allowanceAmount;
+  if (lastAllowedAmount < newMatchAmountInput.value) {
+    approve(ourContractAddress, newMatchAmountInput.value);
+  } else {
+    increaseMaxMatch(encodedName, newMatchAmountInput.value);
+    console.log(` ${newMatchAmountInput.value} amount increased for matching`);
+  }
 }
 
 async function createNewPoolByUser() {

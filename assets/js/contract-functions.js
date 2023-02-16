a770baed3f147701c7634e8d0282cfa905d28d6c
Orientus Prime <dr.orientem@gmail.com>
2023-02-16 09:04:16 +0300

    New js files for refactoring

---
+++ assets/js/contract-functions.js
@@ -0,0 +1,88 @@
+async function getDonationAddressName(address) {
+    const name = await ourContract.donationAddressNames(address);
+    return name;
+}
+
+async function getPoolName(poolId) {
+    const myPoolName = await ourContract.poolNames(poolId);
+    return myPoolName;
+}
+
+async function getDonationAddressCount() {
+    const donationAddressCount = await ourContract.donationAddressCount();
+    return donationAddressCount;
+}
+
+async function getDonationAddress(index) {
+    const donationAddress = await ourContract.donationAddresses(index);
+    return donationAddress;
+}
+
+async function getDonationAddressName(address) {
+    const donationAddressName = await ourContract.donationAddressNames(address);
+    return donationAddressName;
+}
+
+async function encode(name) {
+    const encoded = await ourContract.encode(name);
+    return encoded;
+}
+
+async function getPool(poolId) {
+    let pool = await ourContract.pools(poolId);
+    return pool;
+}
+
+async function donateWithMatch(poolId, amount) {
+    const tx = await ourContract.donateWithMatch(poolId, amount);
+    console.log(`Transaction hash: ${tx.hash}`);
+    return tx;
+}
+
+async function createPool(
+    matchAmount,
+    deadline,
+    foundationDonationAdressId,
+    name
+  ){
+    const tx = await ourContract.createPool(
+        matchAmount,
+        deadline,
+        foundationDonationAdressId,
+        name
+      );
+    console.log(`Transaction hash: ${tx.hash}`);
+    return tx;
+}
+
+async function closePoolWithDonation(poolId) {
+    const tx = await ourContract.closePoolWithDonation(poolId);
+    console.log(`Transaction hash: ${tx.hash}`);
+}
+
+async function closePoolWithWithdraw(poolId) {
+    const tx = await ourContract.closePoolWithWithdraw(poolId);
+    console.log(`Transaction hash: ${tx.hash}`);
+}
+
+async function increaseDeadline(poolId, increaseDeadlineAmount) {
+    const tx = await ourContract.increaseDeadline(poolId, increaseDeadlineAmount);
+    console.log(`Transaction hash: ${tx.hash}`);
+}
+
+async function increaseMaxMatch(poolId, increaseMaxMatchAmount) {
+    const tx = await ourContract.increaseMaxMatch(poolId, increaseMaxMatchAmount);
+    console.log(`Transaction hash: ${tx.hash}`);
+}
+
+async function getAllowance(owner, spender) {
+    let myAllowance = await usdcContract.allowance(owner, spender);
+    console.log("Allowance: " + myAllowance * 1000000);
+    return myAllowance;
+}
+
+async function approve(spender, value) {
+    let myApprove = await usdcContract.approve(spender, value);
+    console.log("Approve: " + myApprove);
+    return myApprove;
+}

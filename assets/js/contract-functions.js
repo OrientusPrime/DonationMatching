027cc1e77bf915a38d32094e328a0fca49fe6ba4
Heval Nergiz <1hevalnergiz1@gmail.com>
2023-02-18 23:37:48 +0300

    max button added navbar created with new buttons

--- assets/js/contract-functions.js
+++ assets/js/contract-functions.js
@@ -1,87 +1,87 @@
 async function getDonationAddressName(address) {
-    const name = await ourContract.donationAddressNames(address);
-    return name;
+  const name = await ourContract.donationAddressNames(address);
+  return name;
 }
 
 async function getPoolName(poolId) {
-    const myPoolName = await ourContract.poolNames(poolId);
-    return myPoolName;
+  const myPoolName = await ourContract.poolNames(poolId);
+  return myPoolName;
 }
 
 async function getDonationAddressCount() {
-    const donationAddressCount = await ourContract.donationAddressCount();
-    return donationAddressCount;
+  const donationAddressCount = await ourContract.donationAddressCount();
+  return donationAddressCount;
 }
 
 async function getDonationAddress(index) {
-    const donationAddress = await ourContract.donationAddresses(index);
-    return donationAddress;
+  const donationAddress = await ourContract.donationAddresses(index);
+  return donationAddress;
 }
 
 async function getDonationAddressName(address) {
-    const donationAddressName = await ourContract.donationAddressNames(address);
-    return donationAddressName;
+  const donationAddressName = await ourContract.donationAddressNames(address);
+  return donationAddressName;
 }
 
 async function encode(name) {
-    const encoded = await ourContract.encode(name);
-    return encoded;
+  const encoded = await ourContract.encode(name);
+  return encoded;
 }
 
 async function getPool(poolId) {
-    let pool = await ourContract.pools(poolId);
-    return pool;
+  let pool = await ourContract.pools(poolId);
+  return pool;
 }
 
 async function donateWithMatch(poolId, amount) {
-    const tx = await ourContract.donateWithMatch(poolId, amount);
-    console.log(`Transaction hash: ${tx.hash}`);
-    return tx;
+  const tx = await ourContract.donateWithMatch(poolId, amount);
+  console.log(`Transaction hash: ${tx.hash}`);
+  return tx;
 }
 
 async function createPool(
+  matchAmount,
+  deadline,
+  foundationDonationAdressId,
+  name
+) {
+  const tx = await ourContract.createPool(
     matchAmount,
     deadline,
     foundationDonationAdressId,
     name
-  ){
-    const tx = await ourContract.createPool(
-        matchAmount,
-        deadline,
-        foundationDonationAdressId,
-        name
-      );
-    console.log(`Transaction hash: ${tx.hash}`);
-    return tx;
+  );
+  console.log(`Transaction hash: ${tx.hash}`);
+  return tx;
 }
 
 async function closePoolWithDonation(poolId) {
-    const tx = await ourContract.closePoolWithDonation(poolId);
+  const tx = await ourContract.closePoolWithDonation(poolId);
 }
 
 async function closePoolWithWithdraw(poolId) {
-    const tx = await ourContract.closePoolWithWithdraw(poolId);
+  const tx = await ourContract.closePoolWithWithdraw(poolId);
 }
 
 async function increaseDeadline(poolId, increaseDeadlineAmount) {
-    const tx = await ourContract.increaseDeadline(poolId, increaseDeadlineAmount);
+  const tx = await ourContract.increaseDeadline(poolId, increaseDeadlineAmount);
 }
 
 async function increaseMaxMatch(poolId, increaseMaxMatchAmount) {
-    const tx = await ourContract.increaseMaxMatch(poolId, increaseMaxMatchAmount);
+  const tx = await ourContract.increaseMaxMatch(poolId, increaseMaxMatchAmount);
 }
 
 async function getAllowance(owner, spender) {
-    let myAllowance = await usdcContract.allowance(owner, spender);
-    return myAllowance;
+  let myAllowance = await usdcContract.allowance(owner, spender);
+  return myAllowance;
 }
 
 async function approve(spender, value) {
-    let myApprove = await usdcContract.approve(spender, value);
-    return myApprove;
+  let myApprove = await usdcContract.approve(spender, value);
+  return myApprove;
 }
 
 async function getDecimals() {
-    let decimals = await usdcContract.decimals();
-    return decimals;
+  let decimals = await usdcContract.decimals();
+  return decimals;
 }
